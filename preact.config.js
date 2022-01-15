dcdf470c84b9aed741d313422a14c91fde34ceb0
Sergey K <hello.to@me.com>
2022-01-16 02:25:22 +0300

    up

--- preact.config.js
+++ preact.config.js
@@ -1,3 +1,21 @@
-export default (config) => {
-    config.output.publicPath = '/arbiape-club-site/';
+// export default (config) => {
+//     config.output.publicPath = '/arbiape-club-site/';
+// };
+export default {
+    webpack(config, env, helpers, options) {
+
+        const publicPath = process.env.GITHUB_PAGES
+            ? `/${process.env.GITHUB_PAGES}/`
+            : '/';
+        const ghEnv = process.env.GITHUB_PAGES
+            && JSON.stringify(`${process.env.GITHUB_PAGES}`);
+
+        config.output.publicPath = publicPath;
+        const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+        Object.assign(
+            plugin.definitions,
+            { ['process.env.GITHUB_PAGES']: ghEnv }
+        );
+
+    },
 };
